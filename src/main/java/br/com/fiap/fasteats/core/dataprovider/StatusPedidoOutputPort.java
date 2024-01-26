260b0216e2a2ac87b16a2d90e208f901fc43ea81
Wesley Gomes <wesleygomes197@gmail.com>
2024-01-26 03:52:18 -0300

    Ajuste geral no projeto

--- src/main/java/br/com/fiap/fasteats/core/dataprovider/StatusPedidoOutputPort.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.fasteats.core.dataprovider;
-
-import br.com.fiap.fasteats.core.domain.model.StatusPedido;
-
-import java.util.Optional;
-
-public interface StatusPedidoOutputPort {
-    Optional<StatusPedido> consultarPorNome(String nome);
-}
--- src/main/java/br/com/fiap/fasteats/core/usecase/StatusPedidoInputPort.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.fasteats.core.usecase;
-
-import br.com.fiap.fasteats.core.domain.model.StatusPedido;
-
-public interface StatusPedidoInputPort {
-    StatusPedido consultarPorNome(String nome);
-}
