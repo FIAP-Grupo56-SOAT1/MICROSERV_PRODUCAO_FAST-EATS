ab7fb63cbbe082df1f751f003abe29b883633b61
Jairo <hardtelles@gmail.com>
2024-03-11 22:52:18 -0300

    saga

--- src/main/java/br/com/fiap/fasteats/config/AwsSQSConfig.java
+++ src/main/java/br/com/fiap/fasteats/config/AwsSQSConfig.java
@@ -1,10 +1,12 @@
 package br.com.fiap.fasteats.config;
 
+import io.awspring.cloud.sqs.listener.QueueNotFoundStrategy;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
@@ -17,6 +19,8 @@ public class AwsSQSConfig {
     private String accessKey;
     @Value("${spring.cloud.aws.credentials.secret-key}")
     private String secretKey;
+    @Value("${spring.cloud.aws.credentials.session-token}")
+    private String sessionToken;
     @Value("${spring.cloud.aws.region.static}")
     private String region;
     @Value("${spring.cloud.aws.sqs.endpoint}")
@@ -24,17 +28,20 @@ public class AwsSQSConfig {
 
     @Bean
     public SqsAsyncClient sqsAsyncClient() {
+        System.out.println("accessKey: " + accessKey);
         return SqsAsyncClient
                 .builder()
                 .endpointOverride(URI.create(endpoint))
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider
-                        .create(AwsBasicCredentials.create(accessKey, secretKey)))
+                        .create(AwsSessionCredentials.create(accessKey, secretKey,sessionToken)))
                 .build();
     }
 
     @Bean
     public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient) {
-        return SqsTemplate.builder().sqsAsyncClient(sqsAsyncClient).build();
+        return SqsTemplate.builder().sqsAsyncClient(sqsAsyncClient)
+                .configure(o -> o.queueNotFoundStrategy(QueueNotFoundStrategy.FAIL))
+                .build();
     }
 }
