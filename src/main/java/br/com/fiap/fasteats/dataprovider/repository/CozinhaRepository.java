f2ad0e874557ec9a6131c75952795eb4ea379f6a
Wesley Gomes <wesleygomes197@gmail.com>
2024-03-16 17:47:17 -0300

    Melhorias no projeto

--- src/main/java/br/com/fiap/fasteats/dataprovider/repository/CozinhaRepository.java
+++ src/main/java/br/com/fiap/fasteats/dataprovider/repository/CozinhaRepository.java
@@ -3,7 +3,6 @@ package br.com.fiap.fasteats.dataprovider.repository;
 
 import br.com.fiap.fasteats.dataprovider.repository.entity.CozinhaPedidoEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
--- src/test/java/br/com/fiap/fasteats/entrypoint/controller/CozinhaPedidoControllerUnitTest.java
+++ src/test/java/br/com/fiap/fasteats/entrypoint/controller/CozinhaPedidoControllerUnitTest.java
@@ -6,6 +6,7 @@ import br.com.fiap.fasteats.entrypoint.controller.mapper.CozinhaPedidoResponseMa
 import br.com.fiap.fasteats.entrypoint.controller.response.CozinhaPedidoResponse;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -16,15 +17,19 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 class CozinhaPedidoControllerUnitTest {
+
     @Mock
     private CozinhaPedidoInputPort cozinhaPedidoInputPort;
+
     @Mock
     private CozinhaPedidoResponseMapper cozinhaPedidoResponseMapper;
+
     @InjectMocks
     private CozinhaPedidoController cozinhaPedidoController;
+
     AutoCloseable openMocks;
 
     @BeforeEach
@@ -37,142 +42,59 @@ class CozinhaPedidoControllerUnitTest {
         openMocks.close();
     }
 
-    /*@Test
-    void receberPedido_DeveRetornarOk_QuandoComunicacaoBemSucedida() {
-        // Arrange
-        Long pedidoId = 1L;
-
-        // Act
-        ResponseEntity<Void> response = cozinhaPedidoController.receberPedido(pedidoId);
-
-        // Assert
-        assertNotNull(response);
-        assertEquals(200, response.getStatusCode().value());
-        verify(cozinhaPedidoInputPort, times(1)).receber(pedidoId);
-    }
-
     @Test
-    void iniciarPreparo_DeveRetornarCozinhaPedidoResponse_QuandoComunicacaoBemSucedida() {
-        // Arrange
-        Long pedidoId = 1L;
-        CozinhaPedido cozinhaPedido = new CozinhaPedido();
-        CozinhaPedidoResponse cozinhaPedidoResponse = new CozinhaPedidoResponse();
-
-        when(cozinhaPedidoInputPort.iniciarPreparo(pedidoId)).thenReturn(cozinhaPedido);
-        when(cozinhaPedidoResponseMapper.toCozinhaPedidoResponse(cozinhaPedido)).thenReturn(cozinhaPedidoResponse);
-
-        // Act
-        ResponseEntity<CozinhaPedidoResponse> response = cozinhaPedidoController.iniciarPreparo(pedidoId);
-
-        // Assert
-        assertNotNull(response);
-        assertEquals(200, response.getStatusCode().value());
-        assertEquals(cozinhaPedidoResponse, response.getBody());
-        verify(cozinhaPedidoInputPort, times(1)).iniciarPreparo(pedidoId);
-        verify(cozinhaPedidoResponseMapper, times(1)).toCozinhaPedidoResponse(cozinhaPedido);
-    }
-
-    @Test
-    void finalizarPreparo_DeveRetornarCozinhaPedidoResponse_QuandoComunicacaoBemSucedida() {
-        // Arrange
-        Long pedidoId = 1L;
-        CozinhaPedido cozinhaPedido = new CozinhaPedido();
-        CozinhaPedidoResponse cozinhaPedidoResponse = new CozinhaPedidoResponse();
-
-        when(cozinhaPedidoInputPort.finalizarPreparo(pedidoId)).thenReturn(cozinhaPedido);
-        when(cozinhaPedidoResponseMapper.toCozinhaPedidoResponse(cozinhaPedido)).thenReturn(cozinhaPedidoResponse);
-
-        // Act
-        ResponseEntity<CozinhaPedidoResponse> response = cozinhaPedidoController.finalizarPreparo(pedidoId);
-
-        // Assert
-        assertNotNull(response);
-        assertEquals(200, response.getStatusCode().value());
-        assertEquals(cozinhaPedidoResponse, response.getBody());
-        verify(cozinhaPedidoInputPort, times(1)).finalizarPreparo(pedidoId);
-        verify(cozinhaPedidoResponseMapper, times(1)).toCozinhaPedidoResponse(cozinhaPedido);
-    }
-
-    @Test
-    void retirarPedido_DeveRetornarCozinhaPedidoResponse_QuandoComunicacaoBemSucedida() {
-        // Arrange
-        Long pedidoId = 1L;
-        CozinhaPedido cozinhaPedido = new CozinhaPedido();
-        CozinhaPedidoResponse cozinhaPedidoResponse = new CozinhaPedidoResponse();
-
-        when(cozinhaPedidoInputPort.retirar(pedidoId)).thenReturn(cozinhaPedido);
-        when(cozinhaPedidoResponseMapper.toCozinhaPedidoResponse(cozinhaPedido)).thenReturn(cozinhaPedidoResponse);
-
-        // Act
-        ResponseEntity<CozinhaPedidoResponse> response = cozinhaPedidoController.retirarPedido(pedidoId);
-
-        // Assert
-        assertNotNull(response);
-        assertEquals(200, response.getStatusCode().value());
-        assertEquals(cozinhaPedidoResponse, response.getBody());
-        verify(cozinhaPedidoInputPort, times(1)).retirar(pedidoId);
-        verify(cozinhaPedidoResponseMapper, times(1)).toCozinhaPedidoResponse(cozinhaPedido);
-    }
-
-    @Test
-    void listar_DeveRetornarListaCozinhaPedidoResponse_QuandoComunicacaoBemSucedida() {
+    @DisplayName("Teste - Listar Pedidos")
+    void deveRetornarListaPedidos_QuandoListar() {
         // Arrange
         List<CozinhaPedido> cozinhaPedidoList = List.of(new CozinhaPedido());
         List<CozinhaPedidoResponse> cozinhaPedidoResponseList = List.of(new CozinhaPedidoResponse());
-
         when(cozinhaPedidoInputPort.listar()).thenReturn(cozinhaPedidoList);
         when(cozinhaPedidoResponseMapper.toCozinhaPedidoResponseList(cozinhaPedidoList)).thenReturn(cozinhaPedidoResponseList);
 
         // Act
-        ResponseEntity<List<CozinhaPedidoResponse>> response = cozinhaPedidoController.listar();
+        ResponseEntity<List<CozinhaPedidoResponse>> result = cozinhaPedidoController.listar();
 
         // Assert
-        assertNotNull(response);
-        assertEquals(200, response.getStatusCode().value());
-        assertEquals(cozinhaPedidoResponseList, response.getBody());
-        verify(cozinhaPedidoInputPort, times(1)).listar();
-        verify(cozinhaPedidoResponseMapper, times(1)).toCozinhaPedidoResponseList(cozinhaPedidoList);
+        assertNotNull(result);
+        assertEquals(200, result.getStatusCodeValue());
+        assertEquals(cozinhaPedidoResponseList, result.getBody());
     }
 
     @Test
-    void consultar_DeveRetornarCozinhaPedidoResponse_QuandoComunicacaoBemSucedida() {
+    @DisplayName("Teste - Consultar Pedido por ID da Cozinha")
+    void deveRetornarPedido_QuandoConsultarPorIdCozinha() {
         // Arrange
         String cozinhaId = "12345";
         CozinhaPedido cozinhaPedido = new CozinhaPedido();
         CozinhaPedidoResponse cozinhaPedidoResponse = new CozinhaPedidoResponse();
-
         when(cozinhaPedidoInputPort.consultar(cozinhaId)).thenReturn(cozinhaPedido);
         when(cozinhaPedidoResponseMapper.toCozinhaPedidoResponse(cozinhaPedido)).thenReturn(cozinhaPedidoResponse);
 
         // Act
-        ResponseEntity<CozinhaPedidoResponse> response = cozinhaPedidoController.consultarPorIdPedido(cozinhaId);
+        ResponseEntity<CozinhaPedidoResponse> result = cozinhaPedidoController.consultarPorIdPedido(cozinhaId);
 
         // Assert
-        assertNotNull(response);
-        assertEquals(200, response.getStatusCode().value());
-        assertEquals(cozinhaPedidoResponse, response.getBody());
-        verify(cozinhaPedidoInputPort, times(1)).consultar(cozinhaId);
-        verify(cozinhaPedidoResponseMapper, times(1)).toCozinhaPedidoResponse(cozinhaPedido);
+        assertNotNull(result);
+        assertEquals(200, result.getStatusCodeValue());
+        assertEquals(cozinhaPedidoResponse, result.getBody());
     }
 
     @Test
-    void consultarPorIdPedido_DeveRetornarCozinhaPedidoResponse_QuandoComunicacaoBemSucedida() {
+    @DisplayName("Teste - Consultar Pedido por ID do Pedido")
+    void deveRetornarPedido_QuandoConsultarPorIdPedido() {
         // Arrange
         Long pedidoId = 1L;
         CozinhaPedido cozinhaPedido = new CozinhaPedido();
         CozinhaPedidoResponse cozinhaPedidoResponse = new CozinhaPedidoResponse();
-
         when(cozinhaPedidoInputPort.consultarPorIdPedido(pedidoId)).thenReturn(cozinhaPedido);
         when(cozinhaPedidoResponseMapper.toCozinhaPedidoResponse(cozinhaPedido)).thenReturn(cozinhaPedidoResponse);
 
         // Act
-        ResponseEntity<CozinhaPedidoResponse> response = cozinhaPedidoController.consultarPorIdPedido(pedidoId);
+        ResponseEntity<CozinhaPedidoResponse> result = cozinhaPedidoController.consultarPorIdPedido(pedidoId);
 
         // Assert
-        assertNotNull(response);
-        assertEquals(200, response.getStatusCode().value());
-        assertEquals(cozinhaPedidoResponse, response.getBody());
-        verify(cozinhaPedidoInputPort, times(1)).consultarPorIdPedido(pedidoId);
-        verify(cozinhaPedidoResponseMapper, times(1)).toCozinhaPedidoResponse(cozinhaPedido);
-    }*/
+        assertNotNull(result);
+        assertEquals(200, result.getStatusCodeValue());
+        assertEquals(cozinhaPedidoResponse, result.getBody());
+    }
 }
